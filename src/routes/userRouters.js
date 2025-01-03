f0014170ba67fae203caab23003e02664dcfab06
ved_patel <ved@auberginesolutions.com>
2025-01-03 12:25:14 +0530

    improve code

--- src/routes/userRouters.js
+++ src/routes/userRouters.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const { validateLogin, validateRegister } = require('../middlewares/userMiddleware');
+
 // Login route
-router.post('/login', validateLogin, (req, res) => {
-    userController.login(req, res);
-});
+router.post('/login', validateLogin, userController.login);
 
 // Register route
-router.post('/register', validateRegister, (req, res) => {
-    userController.register(req, res);
-});
+router.post('/register', validateRegister, userController.register);
 
 module.exports = router;
